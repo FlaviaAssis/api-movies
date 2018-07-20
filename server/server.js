d001690c1b89ac33952854882255495123e7dd01
FlaviaAssis <35305947+FlaviaAssis@users.noreply.github.com>
2018-07-19 20:03:12 -0400

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const restify = require("restify");
+class Server {
+    initRoutes(routers) {
+        return new Promise((resolve, reject) => {
+            try {
+                this.application = restify.createServer({
+                    name: 'meat-api',
+                    version: '1.0.0'
+                });
+                this.application.use(restify.plugins.queryParser());
+                this.application.use(restify.plugins.bodyParser());
+                for (let router of routers) {
+                    router.applyRoutes(this.application);
+                }
+                this.application.listen(8080, () => {
+                    resolve(this.application);
+                });
+            }
+            catch (error) {
+                reject(error);
+            }
+        });
+    }
+    bootstrap(routers = []) {
+        return this.initRoutes(routers).then(() => this);
+    }
+}
+exports.Server = Server;
